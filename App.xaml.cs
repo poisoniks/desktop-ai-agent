ed66cc4745b3915429a68046013cc7e4c226e227
poisoniks <vladremkov888@gmail.com>
2025-09-21 11:59:27 +0300

    Create UI window in tray with stub content

---
+++ App.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using Forms = System.Windows.Forms;
+
+namespace TrayPopupApp
+{
+    public partial class App : System.Windows.Application
+    {
+        private Forms.NotifyIcon? _trayIcon;
+        private MainWindow? _window;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            _window = new MainWindow();
+
+            _trayIcon = new Forms.NotifyIcon
+            {
+                Text = "Tray Popup App",
+                Visible = true,
+                Icon = LoadIconOrFallback()
+            };
+
+            var menu = new Forms.ContextMenuStrip();
+            menu.Items.Add("Show", null, (_, __) => ShowWindow());
+            menu.Items.Add("Hide", null, (_, __) => _window?.Hide());
+            menu.Items.Add(new Forms.ToolStripSeparator());
+            menu.Items.Add("Exit", null, (_, __) => ExitApp());
+            _trayIcon.ContextMenuStrip = menu;
+
+            _trayIcon.MouseClick += (s, args) =>
+            {
+                if (args.Button == Forms.MouseButtons.Left)
+                {
+                    ShowWindow();
+                }
+            };
+
+            ShowWindow();
+        }
+
+        private Icon LoadIconOrFallback()
+        {
+            try
+            {
+                var uri = new Uri("pack://application:,,,/Assets/app.ico", UriKind.Absolute);
+                using var stream = System.Windows.Application.GetResourceStream(uri)?.Stream;
+                if (stream != null)
+                {
+                    return new Icon(stream);
+                }
+            }
+            catch { }
+
+            return (Icon)SystemIcons.Application.Clone();
+        }
+
+        private void ShowWindow()
+        {
+            if (_window == null) return;
+            if (!_window.IsVisible)
+            {
+                _window.Show();
+            }
+            if (_window.WindowState == WindowState.Minimized)
+            {
+                _window.WindowState = WindowState.Normal;
+            }
+            _window.Activate();
+            _window.Focus();
+        }
+
+        private void ExitApp()
+        {
+            try
+            {
+                _trayIcon!.Visible = false;
+                _trayIcon!.Dispose();
+            }
+            catch { }
+
+            _window?.ForceShutdownCleanup();
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            try
+            {
+                _trayIcon?.Dispose();
+            }
+            catch { }
+            base.OnExit(e);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+using System.Windows.Media;
+using Color = System.Windows.Media.Color;
+
+namespace TrayPopupApp
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private const uint MOD_ALT = 0x0001;
+        private const uint MOD_CONTROL = 0x0002;
+        private const uint MOD_NOREPEAT = 0x4000;
+        private const int WM_HOTKEY = 0x0312;
+        private const int HOTKEY_ID = 0xBEEF;
+
+        [DllImport("user32.dll")] private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+        [DllImport("user32.dll")] private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private HwndSource? _hwndSource;
+
+        public ObservableCollection<TabItemVm> Tabs { get; } = new();
+        private TabItemVm? _selectedTab;
+        public TabItemVm? SelectedTab
+        {
+            get => _selectedTab;
+            set { _selectedTab = value; OnPropertyChanged(nameof(SelectedTab)); }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            InitTabs();
+        }
+
+        private void InitTabs()
+        {
+            AddNewTab();
+            Tabs.Add(TabItemVm.AddButton());
+        }
+
+        private void TabButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is FrameworkElement fe && fe.Tag is TabItemVm vm)
+            {
+                if (vm.IsAddButton)
+                {
+                    AddNewTab();
+                }
+                else
+                {
+                    Select(vm);
+                }
+            }
+        }
+
+        private void AddNewTab() 
+        {
+            var idx = Math.Max(0, Tabs.Count - 1);
+            var created = NewContentTab($"Tab {idx}", TabItemVm.CheckIcon());
+            Tabs.Insert(idx, created);
+            Select(created);
+        }
+
+        private void Select(TabItemVm vm)
+        {
+            foreach (var t in Tabs)
+                t.IsSelected = false;
+
+            vm.IsSelected = true;
+            SelectedTab = vm;
+        }
+
+        private TabItemVm NewContentTab(string title, Geometry icon)
+        {
+            var grid = new Grid
+            {
+                Margin = new Thickness(24),
+            };
+            grid.Children.Add(new TextBlock
+            {
+                Text = $"{title}: content here",
+                Foreground = new SolidColorBrush(Color.FromArgb(0xFF, 0xE6, 0xE6, 0xE6)),
+                FontSize = 20
+            });
+
+            return new TabItemVm
+            {
+                Title = title,
+                Icon = icon,
+                Content = grid
+            };
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) => PositionBottomRight();
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var helper = new WindowInteropHelper(this);
+            _hwndSource = HwndSource.FromHwnd(helper.Handle);
+            _hwndSource.AddHook(WndProc);
+
+            _ = RegisterHotKey(helper.Handle, HOTKEY_ID,
+                MOD_CONTROL | MOD_ALT | MOD_NOREPEAT,
+                (uint)System.Windows.Input.KeyInterop.VirtualKeyFromKey(System.Windows.Input.Key.T));
+        }
+
+        private void PositionBottomRight()
+        {
+            const int margin = 10;
+            var workArea = SystemParameters.WorkArea;
+            Left = workArea.Right - Width - margin;
+            Top = workArea.Bottom - Height - margin;
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_HOTKEY && wParam.ToInt32() == HOTKEY_ID)
+            {
+                if (IsVisible && WindowState != WindowState.Minimized)
+                    Hide();
+                else
+                {
+                    if (!IsVisible) Show();
+                    if (WindowState == WindowState.Minimized) WindowState = WindowState.Normal;
+                    PositionBottomRight();
+                    Activate();
+                    Focus();
+                }
+                handled = true;
+            }
+            return IntPtr.Zero;
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            base.OnStateChanged(e);
+            if (WindowState == WindowState.Minimized)
+                Hide();
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+
+        public void ForceShutdownCleanup()
+        {
+            try
+            {
+                var handle = new WindowInteropHelper(this).Handle;
+                if (handle != IntPtr.Zero)
+                    UnregisterHotKey(handle, HOTKEY_ID);
+            }
+            catch { }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    public class TabItemVm : INotifyPropertyChanged
+    {
+        public string Title { get; set; } = "";
+        public Geometry Icon { get; set; } = CheckIcon();
+        public object? Content { get; set; }
+
+        private bool _isSelected;
+        public bool IsSelected { get => _isSelected; set { _isSelected = value; OnPropertyChanged(nameof(IsSelected)); } }
+
+        public bool IsAddButton { get; set; }
+        private bool _isLoading;
+        public bool IsLoading { get => _isLoading; set { _isLoading = value; OnPropertyChanged(nameof(IsLoading)); } }
+
+        public string Tooltip => Title;
+
+        public static Geometry PlusIcon() =>
+            Geometry.Parse("M12,22 L12,12 2,12 2,10 12,10 12,0 14,0 14,10 24,10 24,12 14,12 14,22 Z");
+        public static Geometry CheckIcon() =>
+            Geometry.Parse("M2,12 L5,9 10,14 19,5 22,8 10,20 Z");
+        public static Geometry LoaderStubIcon() =>
+            Geometry.Parse("M12,2 A10,10 0 1 1 11.999,2 Z");
+
+        public static TabItemVm AddButton() => new TabItemVm
+        {
+            Title = "New",
+            Icon = PlusIcon(),
+            IsAddButton = true
+        };
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
